43abfbcad31cf7167d6eba943d30e9e47d4f6282
AuriEcheand <aecheand@gmail.com>
2022-08-21 02:31:30 -0400

    s10-c2 ajout balise article a l'interieur de while fichier front-page.php

--- front-page.php
+++ front-page.php
@@ -5,9 +5,11 @@
     <?php
     if (have_posts()):
         while(have_posts()) : the_post(); ?>
+         <article>
             <h2><a href="<?= get_permalink() ?>"><?php the_field('titre'); ?></a></h2>
             <h3><?php the_field('sous_titre'); ?></h3>
             <p><?php the_field('resume'); ?></p>
+        </article>
         <?php endwhile; ?>
     <?php endif; ?> 
     </div>  
