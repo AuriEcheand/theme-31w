4539ca60e85c496bb78c430759f32012eaf3572c
AuriEcheand <aecheand@gmail.com>
2022-08-21 00:29:03 -0400

    s10-c2 première étape du menu adaptatif .menu__sidebar

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?> 
 <section class="site__main">
-    <h1>front-page.php</h1>
+    <h1>Collège de Maisonneuve</h1>
     <?php
     if (have_posts()):
         while(have_posts()) : the_post(); ?>
